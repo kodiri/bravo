e3dca4eb2dddba558dd6f2151ed1f3f1dbf7f0f7
Sanjay <sanjay.bagtharia@gmail.com>
2019-11-28 18:29:47 +0000

    installed Firebase

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import LandingPage from './LandingPage';
-import Navbar from './pages/lisa_navbar.js';
+//import Navbar from './pages/lisa_navbar.js';
 import './pages/lisa_navbar.css'
 import './App.css';
-import QRCodeForm from './QRCodeForm/QRCodeForm';
+//import QRCodeForm from './QRCodeForm/QRCodeForm';
+import DatabasePage from './DatabaseTest';
 
 export default function App() {
   return (
     <Router>
       <div className='App'>
-        <Navbar />
+        {/* <Navbar /> */}
         <Switch>
           <Route exact path='/' component={LandingPage}/>
+          <Route exact path='/database' component={DatabasePage}/>
         </Switch>
       </div>  
     </Router>
---
+++ src/DatabaseTest.js
@@ -0,0 +1,12 @@
+import React from 'react';
+//import Carousel from "./carousel/SimpleSlider";
+import './App.css';
+
+export default function DatabasePage(){
+    return(
+        <>
+            <p>This page is for database testing</p>
+            {/* <Carousel/> */}
+        </>
+    );
+}
