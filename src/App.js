d0268950a7c59f05604355b3d8cb7b97768c6beb
Sanjay <sanjay.bagtharia@gmail.com>
2019-12-02 13:32:46 +0000

    added database and tested sending data to database

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-// import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import LandingPage from './LandingPage';
-//import Navbar from './pages/lisa_navbar.js';
+import Navbar from './pages/lisa_navbar.js';
 import './pages/lisa_navbar.css'
 import './App.css';
-//import QRCodeForm from './QRCodeForm/QRCodeForm';
-import DatabasePage from './DatabaseTest';
+import QRCodeForm from './QRCodeForm/QRCodeForm';
 
 export default function App() {
   return (
     <Router>
       <div className='App'>
-        {/* <Navbar /> */}
+        <Navbar />
         <Switch>
           <Route exact path='/' component={LandingPage}/>
-          <Route exact path='/database' component={DatabasePage}/>
         </Switch>
       </div>  
     </Router>
--- src/index.js
+++ src/index.js
@@ -3,20 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import * as firebase from 'firebase';
-
-// Your web app's Firebase configuration
-var firebaseConfig = {
-apiKey: "AIzaSyDva7Jz0M4Iu5QzdVt2Uttk-mh9E3qK5-A",
-authDomain: "tap-app-12ee9.firebaseapp.com",
-databaseURL: "https://tap-app-12ee9.firebaseio.com",
-projectId: "tap-app-12ee9",
-storageBucket: "tap-app-12ee9.appspot.com",
-messagingSenderId: "543848800624",
-appId: "1:543848800624:web:96b5a97eece7dceae4efb5"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
