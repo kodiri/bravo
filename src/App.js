45c10f13d36b57fe4c1daeb496a50363ee29ccfe
Lisa Nguyen <lisanguyen@NC.localdomain>
2019-12-02 16:49:50 +0000

    Format NavBars and show footer on all pages

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import Navbar from './NavBar/lisa_navbar.js';
 import './NavBar/lisa_navbar.css'
 import './App.css';
 import QRCodeForm from './QRCodeForm/QRCodeForm';
+import FooterLoggedOut from './Footer/FooterLoggedOut';
 
 export default function App() {
   return (
     <Router>
       <div className='App'>
       <Route path='/:page' component={Navbar}/>
+      <FooterLoggedOut/>
         <Switch>
           <Route exact path='/' component={LandingPage}/>
         </Switch>
--- src/NavBar/lisa_navbar.js
+++ src/NavBar/lisa_navbar.js
@@ -3,34 +3,35 @@ import { useParams } from "react-router";
 import './lisa_navbar.css';
 import logo from '../TAP.png';
 import navSettings from '../icons/settings.svg';
+import navLanding from '../icons/home.svg';
 import landingPage from '../LandingPage';
 
 function NavBar() {
-    
+
     let { page } = useParams();
     console.log('The current page is', page)
     return (
         <nav id='navHeader'>
-            <a className='navLogo' href='logo'><img src={logo} alt='TAP' /></a>
+            <a className='navLogo' href='/'><img src={logo} alt='TAP' /></a>
             <div className='links'>
                 {
                     page === undefined || page === 'landing' ?
                         <>
-                            <div><a href='/login'><p className='navLogIn'>Login</p></a></div>
-                            <div><a href='/signup'><p className='navSignUp'>Sign Up</p></a></div>
+                            <div className='navLogIn'><a href='/login'>Login</a></div>
+                            <div className='navSignUp'><a href='/signup'>Sign Up</a></div>
                         </>
                         :
                         page === 'login' ?
                         <>
-                                <div><a href='/landing'><p className='navLanding'>Landing</p></a></div>
-                                <div><a href='/signup'><p className='navSignUp'>Sign Up</p></a></div>
+                                <div className='navLanding'><a href='/landing'><img src ={navLanding}></img></a></div>
+                                <div className='navSignUp2'><a href='/signup'>Sign Up</a></div>
                                 </> :
                             page === 'signup' ?
                             <>
-                                    <div><a href='/landing'><p className='navLanding'>Landing</p></a></div>
-                                    <div><a href='/login'><p className='navLogIn'>Login</p></a></div>
+                                    <div className='navLanding'><a href='/landing'><img src ={navLanding}></img></a></div>
+                                    <div className='navLogIn2'><a href='/login' >Login</a></div>
                                     </> :
-                                    <div><a href='/settings'><p className='navSettings'><img src ={navSettings}></img></p></a></div>
+                                    <div className='navSettings'><a href='/settings'><img src ={navSettings}></img></a></div>
                 }
             </div>
         </nav>
