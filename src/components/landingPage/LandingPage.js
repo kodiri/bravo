5bf04d185a7ab11cfa54ccdfd7ca212c3b2d14f8
maxgdev <max@apphilia.net>
2019-12-11 15:21:54 +0000

    Added background image, styling to landing page

--- src/components/landingPage/LandingPage.js
+++ src/components/landingPage/LandingPage.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import './LandingPage.css';
 // import React from './node_modules/react';
 
 export default function LandingPage(){
     return(
-        <>
+        <div className='landingPage'>
             <h1>Welcome to TAP</h1>
-            <p>This is the Landing page of the web app TAP!!</p>
-        </>
+            <h3>Your Social & Networking QR Code Creator!</h3>
+        </div>
     );
 }
