96f6c0a9e3c11016a71f7b2bf82964b6ccbed5b5
maxgdev <max@apphilia.net>
2019-12-11 16:12:20 +0000

    Removed background image and replaced with logo image and styled text

--- src/components/landingPage/LandingPage.js
+++ src/components/landingPage/LandingPage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './LandingPage.css';
+import tap from './tap.png'
 // import React from './node_modules/react';
 
 export default function LandingPage(){
     return(
         <div className='landingPage'>
             <h1>Welcome to TAP</h1>
+            <img  src={tap}/>
             <h3>Your Social & Networking QR Code Creator!</h3>
         </div>
     );
