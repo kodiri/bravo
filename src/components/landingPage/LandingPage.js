6c310f3dae740d9e62e1bca5ee11f756ce6e18c0
Lisa Nguyen <lisanguyen@Lisas-MacBook-Pro.local>
2019-12-11 09:24:27 -0800

    Create landing page

--- src/components/landingPage/LandingPage.js
+++ src/components/landingPage/LandingPage.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Logo from './TAP.png';
+import './LandingPage.css';
 // import React from './node_modules/react';
 
-export default function LandingPage(){
-    return(
-        <>
-            <h1>Welcome to TAP</h1>
-            <p>This is the Landing page of the web app TAP!!</p>
-        </>
+export default function LandingPage() {
+    return (
+        <div className='landingContainer'><img className='TAPLogoLanding'src={Logo} alt='TAP'></img>
+        <br></br><br></br><br></br>
+            <button className='landingButton' onClick>START TAPPING</button></div>
     );
 }
