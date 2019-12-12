ee42677a75bc5d54e89bf31dc6d8a2f2aad1d946
Lisa Nguyen <lisanguyen@Lisas-MacBook-Pro.local>
2019-12-11 20:59:13 -0800

    Fix and format terms

--- src/components/terms/Terms.js
+++ src/components/terms/Terms.js
@@ -1,92 +1,89 @@
 import React from 'react';
+import './Terms.css';
 
 export default function Terms() {
     return (
         <>
-            <div>
+            <div className='terms'>
                 <h1>TERMS AND CONDITIONS</h1>
-                    <h2>https://state.now.sh</h2>
-                    <h3>Last Updated December 9th, 2019</h3>
-                    <br></br>
+                <h3>https://state.now.sh</h3>
+                <h3>Last Updated December 9th, 2019</h3>
 
-                    <h2>PLEASE READ THESE TERMS AND CONDITIONS CAREFULLY</h2>
-                    <br></br>
-                    <h2>AGREEMENT TO TERMS</h2>
-                    <br></br>
-                    <p>These Terms and Conditions constitute a legally binding agreement made between you, whether personally or on behalf of an entity (“you”) and TAP (“we,” “us” or “our”), concerning your access to and use of the https://state.now.sh website as well as any other media form, media channel, mobile website or mobile application related, linked, or otherwise connected thereto (collectively, the “Site”). You agree that by accessing the Site, you have read, understood, and agree to be bound by all of these Terms and Conditions Use. IF YOU DO NOT AGREE WITH ALL OF THESE TERMS and CONDITIONS, THEN YOU ARE EXPRESSLY PROHIBITED FROM USING THE SITE AND YOU MUST DISCONTINUE USE IMMEDIATELY.</p>
-                    <br></br>
-                    <p>Supplemental terms and conditions or documents that may be posted on the Site from time to time are hereby expressly incorporated herein by reference. We reserve the right, in our sole discretion, to make changes or modifications to these Terms and Conditions at any time and for any reason. We will alert you about any changes by updating the “Last updated” date of these Terms and Conditions and you waive any right to receive specific notice of each such change. It is your responsibility to periodically review these Terms and Conditions to stay informed of updates. You will be subject to, and will be deemed to have been made aware of and to have accepted, the changes in any revised Terms and Conditions by your continued use of the Site after the date such revised Terms are posted.</p>
-                    <br></br>
-                    <p>The information provided on the Site is not intended for distribution to or use by any person or entity in any jurisdiction or country where such distribution or use would be contrary to law or regulation or which would subject us to any registration requirement within such jurisdiction or country. Accordingly, those persons who choose to access the Site from other locations do so on their own initiative and are solely responsible for compliance with local laws, if and to the extent local laws are applicable.</p>
-                    <br></br>
-                    <h2>INTELLECTUAL PROPERTY RIGHTS</h2>
-                    <br></br>
-                    <p>Unless otherwise indicated, the Site is our proprietary property and all source code, databases, functionality, software, website designs, audio, video, text, photographs, and graphics on the Site (collectively, the “Content”) and the trademarks, service marks, and logos contained therein (the “Marks”) are owned or controlled by us or licensed to us, and are protected by copyright and trademark laws and various other intellectual property rights and unfair competition laws of the United States, foreign jurisdictions, and international conventions. The Content and the Marks are provided on the Site “AS IS” for your information and personal use only. Except as expressly provided in these Terms of Use, no part of the Site and no Content or Marks may be copied, reproduced, aggregated, republished, uploaded, posted, publicly displayed, encoded, translated, transmitted, distributed, sold, licensed, or otherwise exploited for any commercial purpose whatsoever, without our express prior written permission.</p>
-                    <br></br>
-                    <p>Provided that you are eligible to use the Site, you are granted a limited license to access and use the Site and to download or print a copy of any portion of the Content to which you have properly gained access solely for your personal, non-commercial use. We reserve all rights not expressly granted to you in and to the Site, Content and the Marks.</p>
-                    <br></br>
-                    <h2>USER REGISTRATION</h2>
-                    <br></br>
-                    <p>You may be required to register with the Site. You agree to keep your password confidential and will be responsible for all use of your account and password. We reserve the right to remove, reclaim, or change a username you select if we determine, in our sole discretion, that such username is inappropriate, obscene, or otherwise objectionable.</p>
-                    <br></br>
-                    <h2>PROHIBITED ACTIVITIES</h2>
-                    <br></br>
-                    <p>You may not access or use the Site for any purpose other than that for which we make the Site available. The Site may not be used in connection with any commercial endeavors except those that are specifically endorsed or approved by us.</p>
-                    <br></br>
-                    <p>As a user of the Site, you agree not to:</p>
-                    <br></br>
-                    <ol><li>1.	systematically retrieve data or other content from the Site to create or compile, directly or indirectly, a collection, compilation, database, or directory without written permission from us.</li>
-                        <br></br>
-                        <li>2.	make any unauthorized use of the Site, including collecting usernames and/or email addresses of users by electronic or other means for the purpose of sending unsolicited email, or creating user accounts by automated means or under false pretenses.</li>
-                        <br></br>
-                        <li>3.	use a buying agent or purchasing agent to make purchases on the Site.</li>
-                        <br></br>
-                        <li>4.	use the Site to advertise or offer to sell goods and services.</li>
-                        <br></br>
-                        <li>5.	circumvent, disable, or otherwise interfere with security-related features of the Site, including features that prevent or restrict the use or copying of any Content or enforce limitations on the use of the Site and/or the Content contained therein.</li>
-                        <br></br>
-                        <li>6.	engage in unauthorized framing of or linking to the Site.</li>
-                        <br></br>
-                        <li>7.	trick, defraud, or mislead us and other users, especially in any attempt to learn sensitive account information such as user passwords;</li>
-                        <br></br>
-                        <li>8.	make improper use of our support services or submit false reports of abuse or misconduct.</li>
-                        <br></br>
-                        <li>9.	engage in any automated use of the system, such as using scripts to send comments or messages, or using any data mining, robots, or similar data gathering and extraction tools.</li>
-                        <br></br>
-                        <li>10.	interfere with, disrupt, or create an undue burden on the Site or the networks or services connected to the Site.</li>
-                        <br></br>
-                        <li>11.	attempt to impersonate another user or person or use the username of another user.</li>
-                        <br></br>
-                        <li>12.	sell or otherwise transfer your profile.</li>
-                        <br></br>
-                        <li>13.	use any information obtained from the Site in order to harass, abuse, or harm another person.</li>
-                        <br></br>
-                        <li>14.	use the Site as part of any effort to compete with us or otherwise use the Site and/or the Content for any revenue-generating endeavor or commercial enterprise.</li>
-                        <br></br>
-                        <li>15.	decipher, decompile, disassemble, or reverse engineer any of the software comprising or in any way making up a part of the Site.</li>
-                        <br></br>
-                        <li>16.	attempt to bypass any measures of the Site designed to prevent or restrict access to the Site, or any portion of the Site.</li>
-                        <br></br>
-                        <li>17.	harass, annoy, intimidate, or threaten any of our employees or agents engaged in providing any portion of the Site to you.</li>
-                        <br></br>
-                        <li>18.	delete the copyright or other proprietary rights notice from any Content.</li>
-                        <br></br>
-                        <li>19.	copy or adapt the Site’s software, including but not limited to Flash, PHP, HTML, JavaScript, or other code.</li>
-                        <br></br>
-                        <li>20.	upload or transmit (or attempt to upload or to transmit) viruses, Trojan horses, or other material, including excessive use of capital letters and spamming (continuous posting of repetitive text), that interferes with any party’s uninterrupted use and enjoyment of the Site or modifies, impairs, disrupts, alters, or interferes with the use, features, functions, operation, or maintenance of the Site.</li>
-                        <br></br>
-                        <li>21.	upload or transmit (or attempt to upload or to transmit) any material that acts as a passive or active information collection or transmission mechanism, including without limitation, clear graphics interchange formats (“gifs”), 1×1 pixels, web bugs, cookies, or other similar devices (sometimes referred to as “spyware” or “passive collection mechanisms” or “pcms”).</li>
-                        <br></br>
-                        <li>22.	except as may be the result of standard search engine or Internet browser usage, use, launch, develop, or distribute any automated system, including without limitation, any spider, robot, cheat utility, scraper, or offline reader that accesses the Site, or using or launching any unauthorized script or other software.</li>
-                        <br></br>
-                        <li>23.	disparage, tarnish, or otherwise harm, in our opinion, us and/or the Site</li>
-                        <br></br>
-                        <li>24.	use the Site in a manner inconsistent with any applicable laws or regulations.</li>
-                        <br></br>
-                    </ol>
-                <br></br>
-                <br></br>
-                <br></br>
+
+                <h3>PLEASE READ THESE TERMS AND CONDITIONS CAREFULLY</h3>
+
+                <h2>AGREEMENT TO TERMS</h2>
+
+                <p>These Terms and Conditions constitute a legally binding agreement made between you, whether personally or on behalf of an entity (“you”) and TAP (“we,” “us” or “our”), concerning your access to and use of the https://state.now.sh website as well as any other media form, media channel, mobile website or mobile application related, linked, or otherwise connected thereto (collectively, the “Site”). You agree that by accessing the Site, you have read, understood, and agree to be bound by all of these Terms and Conditions Use. IF YOU DO NOT AGREE WITH ALL OF THESE TERMS and CONDITIONS, THEN YOU ARE EXPRESSLY PROHIBITED FROM USING THE SITE AND YOU MUST DISCONTINUE USE IMMEDIATELY.</p>
+
+                <p>Supplemental terms and conditions or documents that may be posted on the Site from time to time are hereby expressly incorporated herein by reference. We reserve the right, in our sole discretion, to make changes or modifications to these Terms and Conditions at any time and for any reason. We will alert you about any changes by updating the “Last updated” date of these Terms and Conditions and you waive any right to receive specific notice of each such change. It is your responsibility to periodically review these Terms and Conditions to stay informed of updates. You will be subject to, and will be deemed to have been made aware of and to have accepted, the changes in any revised Terms and Conditions by your continued use of the Site after the date such revised Terms are posted.</p>
+
+                <p>The information provided on the Site is not intended for distribution to or use by any person or entity in any jurisdiction or country where such distribution or use would be contrary to law or regulation or which would subject us to any registration requirement within such jurisdiction or country. Accordingly, those persons who choose to access the Site from other locations do so on their own initiative and are solely responsible for compliance with local laws, if and to the extent local laws are applicable.</p>
+
+                <h2>INTELLECTUAL PROPERTY RIGHTS</h2>
+
+                <p>Unless otherwise indicated, the Site is our proprietary property and all source code, databases, functionality, software, website designs, audio, video, text, photographs, and graphics on the Site (collectively, the “Content”) and the trademarks, service marks, and logos contained therein (the “Marks”) are owned or controlled by us or licensed to us, and are protected by copyright and trademark laws and various other intellectual property rights and unfair competition laws of the United States, foreign jurisdictions, and international conventions. The Content and the Marks are provided on the Site “AS IS” for your information and personal use only. Except as expressly provided in these Terms of Use, no part of the Site and no Content or Marks may be copied, reproduced, aggregated, republished, uploaded, posted, publicly displayed, encoded, translated, transmitted, distributed, sold, licensed, or otherwise exploited for any commercial purpose whatsoever, without our express prior written permission.</p>
+
+                <p>Provided that you are eligible to use the Site, you are granted a limited license to access and use the Site and to download or print a copy of any portion of the Content to which you have properly gained access solely for your personal, non-commercial use. We reserve all rights not expressly granted to you in and to the Site, Content and the Marks.</p>
+
+                <h2>USER REGISTRATION</h2>
+
+                <p>You may be required to register with the Site. You agree to keep your password confidential and will be responsible for all use of your account and password. We reserve the right to remove, reclaim, or change a username you select if we determine, in our sole discretion, that such username is inappropriate, obscene, or otherwise objectionable.</p>
+
+                <h2>PROHIBITED ACTIVITIES</h2>
+
+                <p>You may not access or use the Site for any purpose other than that for which we make the Site available. The Site may not be used in connection with any commercial endeavors except those that are specifically endorsed or approved by us.</p>
+
+                <p>As a user of the Site, you agree not to:</p>
+
+                <ol><li>systematically retrieve data or other content from the Site to create or compile, directly or indirectly, a collection, compilation, database, or directory without written permission from us.</li>
+
+                    <li>make any unauthorized use of the Site, including collecting usernames and/or email addresses of users by electronic or other means for the purpose of sending unsolicited email, or creating user accounts by automated means or under false pretenses.</li>
+
+                    <li>use a buying agent or purchasing agent to make purchases on the Site.</li>
+
+                    <li>use the Site to advertise or offer to sell goods and services.</li>
+
+                    <li>circumvent, disable, or otherwise interfere with security-related features of the Site, including features that prevent or restrict the use or copying of any Content or enforce limitations on the use of the Site and/or the Content contained therein.</li>
+
+                    <li>engage in unauthorized framing of or linking to the Site.</li>
+
+                    <li>trick, defraud, or mislead us and other users, especially in any attempt to learn sensitive account information such as user passwords;</li>
+
+                    <li>make improper use of our support services or submit false reports of abuse or misconduct.</li>
+
+                    <li>engage in any automated use of the system, such as using scripts to send comments or messages, or using any data mining, robots, or similar data gathering and extraction tools.</li>
+
+                    <li>interfere with, disrupt, or create an undue burden on the Site or the networks or services connected to the Site.</li>
+
+                    <li>attempt to impersonate another user or person or use the username of another user.</li>
+
+                    <li>sell or otherwise transfer your profile.</li>
+
+                    <li>use any information obtained from the Site in order to harass, abuse, or harm another person.</li>
+
+                    <li>use the Site as part of any effort to compete with us or otherwise use the Site and/or the Content for any revenue-generating endeavor or commercial enterprise.</li>
+
+                    <li>decipher, decompile, disassemble, or reverse engineer any of the software comprising or in any way making up a part of the Site.</li>
+
+                    <li>attempt to bypass any measures of the Site designed to prevent or restrict access to the Site, or any portion of the Site.</li>
+
+                    <li>harass, annoy, intimidate, or threaten any of our employees or agents engaged in providing any portion of the Site to you.</li>
+
+                    <li>delete the copyright or other proprietary rights notice from any Content.</li>
+
+                    <li>copy or adapt the Site’s software, including but not limited to Flash, PHP, HTML, JavaScript, or other code.</li>
+
+                    <li>upload or transmit (or attempt to upload or to transmit) viruses, Trojan horses, or other material, including excessive use of capital letters and spamming (continuous posting of repetitive text), that interferes with any party’s uninterrupted use and enjoyment of the Site or modifies, impairs, disrupts, alters, or interferes with the use, features, functions, operation, or maintenance of the Site.</li>
+
+                    <li>upload or transmit (or attempt to upload or to transmit) any material that acts as a passive or active information collection or transmission mechanism, including without limitation, clear graphics interchange formats (“gifs”), 1×1 pixels, web bugs, cookies, or other similar devices (sometimes referred to as “spyware” or “passive collection mechanisms” or “pcms”).</li>
+
+                    <li>except as may be the result of standard search engine or Internet browser usage, use, launch, develop, or distribute any automated system, including without limitation, any spider, robot, cheat utility, scraper, or offline reader that accesses the Site, or using or launching any unauthorized script or other software.</li>
+
+                    <li>disparage, tarnish, or otherwise harm, in our opinion, us and/or the Site</li>
+
+                    <li>use the Site in a manner inconsistent with any applicable laws or regulations.</li>
+                </ol>
             </div>
         </>
     );
