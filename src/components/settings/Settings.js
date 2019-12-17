dd9a02e0c8144f418b6782e424a8fb867281631d
Jonathan J S <jonatelo.92@gmail.com>
2019-12-17 15:44:43 +0000

    set the settings page with buttons to aboutapp, aboutus, terms pages

--- src/components/settings/Settings.js
+++ src/components/settings/Settings.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import './Settings.css';
+import { Link } from 'react-router-dom';
 
 const Settings = () => {
     return (
-        <div>
-            <h1>This is the Settings page</h1>
+        <div className='mainContainer'>
+            <div className='container'>
+                <div className='aboutAppContainer'>
+                    <h2>About App</h2>
+                    <Link to='/about-app'>
+                        <button className='settingsButton'>About App</button>
+                    </Link>
+                </div>
+                <div className='aboutUsContainer'>
+                    <h2>About Us</h2>
+                    <Link to='/about-us'>
+                    <button className='settingsButton'>About Us</button>
+                    </Link>
+                </div>
+                <div className='termsConditionsContainer'>
+                    <h2>Terms and Conditions</h2>
+                    <Link to='/terms'>
+                    <button className='settingsButton'>Ts & Cs</button>
+                    </Link>
+                </div>
+            </div>
         </div>
     );
 };
